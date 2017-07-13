c1faee155b351187d4b556b6675f66d8e806e021
Mehul Popat <mehul.popat@ge.com>
2017-07-13 14:26:14 +0530

    added URL for movie and products Image

--- src/main/java/com/cynosure/pojo/Product.java
+++ src/main/java/com/cynosure/pojo/Product.java
@@ -5,6 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.apache.commons.lang.StringUtils;
 
 @Entity
 @Table(name="t_product_details", schema="[cynosure-schema]")
@@ -29,6 +32,12 @@ public class Product {
 	
 	@Column(name="STOCK")
 	private Long stock;
+	
+	@Column(name="product_image")
+	private String productImage;
+	
+	@Transient
+	private String productImageFullUrl;
 
 	public Long getProductId() {
 		return productId;
@@ -77,6 +86,24 @@ public class Product {
 	public void setStock(Long stock) {
 		this.stock = stock;
 	}
-	
-	
+
+	public String getProductImage() {
+		return productImage;
+	}
+
+	public void setProductImage(String productImage) {
+		this.productImage = productImage;
+	}
+
+	public String getProductImageFullUrl() {
+		if(StringUtils.isEmpty(productImage)){
+			return "";
+		}else{
+			return "https://cynosureblob.blob.core.windows.net/cynosure-movies/" + productImage;
+		}
+	}
+
+	public void setProductImageFullUrl(String productImageFullUrl) {
+		this.productImageFullUrl = productImageFullUrl;
+	}
 }
