cf8c893052343a1b072a44ac5bbdc43849c463f9
Urman Ratneshwar <urman.ratneshwar@tcs.com>
2017-07-13 22:28:15 +0530

    DailyMailScheduler

--- src/main/java/com/cynosure/util/DailyMailScheduler.java
+++ src/main/java/com/cynosure/util/DailyMailScheduler.java
@@ -3,9 +3,11 @@ package com.cynosure.util;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import com.cynosure.services.ImailService;
 
+@Component
 public class DailyMailScheduler {
 	
 	@Autowired
@@ -14,8 +16,9 @@ public class DailyMailScheduler {
 	public static Logger LOGGER = Logger.getLogger(DailyMailScheduler.class);
 	
 	@Scheduled(cron="0 0 * * *")
-    public void printMessage() {
+    public void sendDailyMail() {
         try {
+        	LOGGER.info("Sending Daily Mail");
 			mailJetMailService.sendDailyMail();
 		} catch (Exception e) {
 			LOGGER.error(e.getMessage());
